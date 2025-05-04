01d836541b764b6aea6fc7210b8fd9aa37a36b4e
Jameel003-5 <abdallahjamel58@gmail.com>
2025-05-05 02:43:33 +0300

    Add config.dart 1746402216483

---
+++ config.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class UiConfig{
+  static Color colorPrime = Color(0xffD9D9D9);
+  static Color colorSec = Color(0xff93BEB1);
+  static Color dark = Color(0xff333333);
+
+  static String? fontPrime = 'Nunito';
+  static String? fontArabic = 'Nunito';
+}
